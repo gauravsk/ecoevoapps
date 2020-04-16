00ab8524fa216026f582f48ccae09526915e95ca
Gaurav Kandlikar <gaurav.kandlikar@gmail.com>
2020-04-15 20:54:08 -0700

    change filepath in run_singlepop_file() yet again

--- R/run_singlepop_document.R
+++ R/run_singlepop_document.R
@@ -1,7 +1,8 @@
 #' @export
 run_singlepop_file <- function() {
   rmarkdown::run(
-    file = "inst/single_population/single_populations_app.Rmd",
+    file = system.file("single_population/single_populations_app.Rmd",
+                       package =  "ecoevoapps"),
     default_file = NULL,
     auto_reload = TRUE,
     shiny_args = NULL,
