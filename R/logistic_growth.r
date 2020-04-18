9eeb6870050f8737e5fc17d12af2ec3eed923836
Gaurav Kandlikar <gaurav.kandlikar@gmail.com>
2020-04-18 11:52:15 -0700

    add a function for lagged logistic growth

--- R/logistic_growth.r
+++ R/logistic_growth.r
@@ -1,4 +1,4 @@
-#' Exponential growth model
+#' Logistic growth model
 #' @param time vector of time units over which to run model
 #' @param init initial population size of population
 #' @param params vector of r (intrinsic growth rate) and K (carrying capacity)
@@ -13,3 +13,23 @@ logistic_growth <- function(time,init,params) {
     return(list(c(dN1)))
   })
 }
+
+
+#' Lagged logistic growth model
+#' @param time vector of time units over which to run model
+#' @param init initial population size of population
+#' @param params vector of r (intrinsic growth rate), K (carrying capacity), and tau (time lag)
+#' @export
+lagged_logistic_growth <- function(time, init, params) {
+  with (as.list(c(time,init,params)), {
+    tlag <- time - tau
+    if (tlag < 0) {
+      ylag <- N1
+    } else {
+      ylag <- lagvalue(tlag)
+    }
+    dN1 <- r * N1[1] * (1-(ylag[1]/K))
+    return(list(c(dN1)))
+  })
+}
+
