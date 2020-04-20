d62468e0cf960a00ffff7b764e42564b8a47aa21
Maddi Cowen <mcowen@ucla.edu>
2020-04-20 16:58:15 -0700

    add SIS model

--- R/SIR_ODE_variants.R
+++ R/SIR_ODE_variants.R
@@ -10,7 +10,7 @@ SIR <- function(time,init,params) {
     # m is the natural mortality/birth rate
     # v is the newborn vaccination rate
     # gamma is the recovery rate
-    dS_dt = m*(S + I + R)(1 - v) - m*S - beta*S*I
+    dS_dt = m*(S + I + R)*(1 - v) - m*S - beta*S*I
     dI_dt = beta*S*I - m*I - gamma*I
     dR_dt = gamma*I - m*R + m*(S + I + R)*v
     return(list(c(dS = dS_dt, dI = dI_dt, dR = dR_dt)))
@@ -47,7 +47,7 @@ SEIR <- function(time,init,params) {
     # m is the natural mortality/birth rate
     # v is the newborn vaccination rate
     # gamma is the recovery rate
-    dS_dt = m*(S + E + I + R)(1 - v) - m*S - beta*S*I
+    dS_dt = m*(S + E + I + R)*(1 - v) - m*S - beta*S*I
     dE_dt = beta*S*I - a*E - m*E
     dI_dt = a*E - m*I - gamma*I
     dR_dt = gamma*I - m*R + m*(S + I + R)*v
@@ -67,10 +67,27 @@ SIRD <- function(time,init,params) {
     # m is the natural mortality/birth rate
     # v is the newborn vaccination rate
     # gamma is the recovery rate
-    dS_dt = m*(S + I + R)(1 - v) - m*S - beta*S*I
+    dS_dt = m*(S + I + R)*(1 - v) - m*S - beta*S*I
     dI_dt = beta*S*I - m*I - gamma*I - mu*I
     dR_dt = gamma*I - m*R + m*(S + I + R)*v
     dD_dt = mu*I
     return(list(c(dS = dS_dt, dI = dI_dt, dR = dR_dt, dD = dD_dt)))
+  })
+}
+
+#' SIS model with vital rates
+#' @param time vector of time units over which to run model
+#' @param init initial population size of population
+#' @param params vector of beta (infection rate), m(natural birth/death rate), and gamma (recovery rate)
+#' @export
+SIS <- function(time,init,params) {
+  with (as.list(c(time,init,params)), {
+    # description of parameters:
+    # beta is the infection rate
+    # m is the natural mortality/birth rate
+    # gamma is the recovery rate
+    dS_dt = m*(S + I) - m*S - beta*S*I + gamma*I
+    dI_dt = beta*S*I - m*I - gamma*I
+    return(list(c(dS = dS_dt, dI = dI_dt)))
   })
 }
