fc1ff1c31b07165867ede965f74a18238cca193b
Maddi Cowen <mcowen@ucla.edu>
2020-05-27 17:09:54 -0700

    add abundance thru time plot for SIS

--- inst/SIR/SIR model.R
+++ inst/SIR/SIR model.R
@@ -4,7 +4,7 @@ library(ecoevoapps)
 
 # source("~/Dropbox/UCLA/ecoevo_models/R/SIR_ODE_variants.R")
 
-##### run SIR model without vaccination #####
+##### run SIR model without vaccination -------------
 # Set the initial population sizes
 init <- c(S = 100, I = 1, R = 0)
 # Set the parameter values
@@ -18,7 +18,8 @@ time <- seq(0,100,by = .1)
 out <- data.frame(deSolve::ode(func = SIR,
                                   y=init, parms=params, times = time))
 
-# Reshape the data so that population sizes of both
+# Reshape the data for plotting -----------------------
+# so that population sizes of both
 # species are in one column, and an extra column to define
 # species name. This helps with the plotting...
 out_long <- pivot_longer(out, c(S, I, R), "group") %>%
@@ -36,6 +37,7 @@ pop_out_long <- pop_out %>%
   pivot_longer(c(dS, dI, dR), "group") %>%
   mutate(group = factor(group, levels = c("dS", "dI", "dR")))
 
+# make plots ------------------------
 # Plot abundance through time
 ggplot(out_long) +
   geom_line(aes(x = time, y = value, color = group), size = 2) +
@@ -78,7 +80,7 @@ ggplot(pop_out_long) +
   ecoevoapps::theme_apps()
 
 
-##### run SIS model without vaccination #####
+##### run SIS model without vaccination -------------------
 # Set the initial population sizes
 init <- c(S = 50, I = 20)
 # Set the parameter values
@@ -92,18 +94,37 @@ time <- seq(0,100,by = .1)
 out <- data.frame(deSolve::ode(func = SIS,
                                y=init, parms=params, times = time))
 
-# Reshape the data so that population sizes of both
+# Reshape the data for plotting --------------------------
+# so that population sizes of both
 # species are in one column, and an extra column to define
 # species name. This helps with the plotting...
 out_long <- pivot_longer(out, c(S, I), "group") %>%
   mutate(group = factor(group, levels = c("S", "I")))
 
+# use out to create dS, dI, dR
+pop_out <- out
+pop_out$dS <- c(NA, diff(pop_out$S))
+pop_out$dI <- c(NA, diff(pop_out$I))
+pop_out <- pop_out %>% mutate(pgrS = dS/S, pgrI = dI/I)
 
+pop_out_long <- pop_out %>%
+  select(time, dS, dI) %>%
+  pivot_longer(c(dS, dI), "group") %>%
+  mutate(group = factor(group, levels = c("dS", "dI")))
+
+# make plots ---------------------------
 # Plot abundance through time
 ggplot(out_long) +
   geom_line(aes(x = time, y = value, color = group), size = 2) +
   scale_color_brewer(palette = "Set1") +
   ylab("Population size") +
+  ecoevoapps::theme_apps()
+
+# Plot dS, dI over time
+ggplot(pop_out_long) +
+  geom_line(aes(x = time, y = value, color = group), size = 2) +
+  scale_color_brewer(palette = "Set1") +
+  ylab("Change in population size") +
   ecoevoapps::theme_apps()
 
 # Plot S vs I
