6ff01b374e6c190dcf6622361d8a2f72701cd9e9
Marcel Vaz <mcvaz@ucla.edu>
2020-06-03 22:53:08 -0700

    open structured population model

---
+++ inst/structured_population/app.R
@@ -0,0 +1,10 @@
+matrixInput(inputId,
+                        value = matrix("", 1, 1),
+                        inputClass = "",
+                        rows = list(),
+                        cols = list(),
+                        class = "character",
+                        paste = FALSE,
+                        copy = FALSE){
+  [...]
+}
---
+++ inst/structured_population/test.R
@@ -0,0 +1,15 @@
+### INPUT
+# Initial sizes
+Ni1 = 100
+Ni2 = 100
+Ni3 = 100
+
+# Leslie matrix
+
+
+
+
+# Population growth
+
+
+
