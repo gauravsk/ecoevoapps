b2ffdeeaf16a5df233a4bcbf360f4b4a0694b773
Maddi Cowen <mcowen@ucla.edu>
2020-06-13 09:51:17 -0700

    change theme_apps function to also have plots start at origin

--- R/theme_apps.R
+++ R/theme_apps.R
@@ -1,6 +1,7 @@
 #' @export
-theme_apps <- function() {
-  ggplot2::theme_minimal() +
+theme_apps <- function(plot) {
+  plot +
+    ggplot2::theme_minimal() +
     ggplot2::theme(panel.border = ggplot2::element_blank(),
           panel.grid.major = ggplot2::element_blank(),
           panel.grid.minor = ggplot2::element_blank(),
@@ -8,7 +9,9 @@ theme_apps <- function() {
           plot.tag = ggplot2::element_text(face = "bold"),
           axis.title = ggplot2::element_text(size = 15),
           plot.caption = ggplot2::element_text(size = 15)
-    )
+    ) +
+    ggplot2::scale_x_continuous(expand = c(0, 1)) +
+    ggplot2::scale_y_continuous(expand = c(0, 1))
 }
 
 
