39d999ea64e6db3f7b100694e662a04f18327a7a
Gaurav Kandlikar <gaurav.kandlikar@gmail.com>
2020-06-15 07:07:26 -0700

    converting ibiogeo app to rmd; cleaning functions etc

---
+++ R/ibiogeo_functions.R
@@ -0,0 +1,45 @@
+#' Immigration rate for Island Biogeography model
+#' @param S number of species on island
+#' @param D distance from mainland (km)
+#' @param M number of species in the mainland
+#' @param k scaling constant
+#' @export
+ibiogeo_I <- function(S,D,M,k){
+  exp(-(k/D)*(S-M))-1
+}
+
+#' Extinction rate for Island Biogeography model
+#' @param S number of species on island
+#' @param A size of island (km^2)
+#' @param M number of species in the mainland
+#' @param k scaling constant
+#' @export
+ibiogeo_E <- function(S,A,M,k) {
+  exp(k*S/A)-1
+}
+
+#' Equilibrium number of species on island for Island Biogeography model
+#' @param D distance from mainland (km)
+#' @param A size of island (km^2)
+#' @param M number of species in the mainland
+#' @param k scaling constant
+#' @export
+ibiogeo_Sx <- function(D,A,M,k){
+  A*M/(D+A)
+}
+
+#' equilibrium immigration rate for Island Biogeography model
+#' @param D distance from mainland (km)
+#' @param A size of island (km^2)
+#' @export
+ibiogeo_Ix <- function(D,A){
+  exp(-(k/D)*(Sx(D,A)-M))-1
+}
+
+#' equilibrium extinction rate for Island Biogeography model
+#' @param D distance from mainland (km)
+#' @param A size of island (km^2)
+#' @export
+ibiogeo_Ex <- function(D,A){
+  exp(k*A*Sx(D,A))-1
+}
