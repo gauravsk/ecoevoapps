267c41a9013035f7c601a9a89abcde3cd40c56c5
Gaurav Kandlikar <gaurav.kandlikar@gmail.com>
2020-06-16 10:18:13 -0700

    WIP; making the two essential nutrients app

--- R/abiotic_resource_competition.R
+++ R/abiotic_resource_competition.R
@@ -1,24 +1,37 @@
-#' Exponential growth model
+#' Competition between two species for two essential resources
 #' @param time vector of time units over which to run model
 #' @param init initial population size of population
 #' @param params vector of parameters: intrinsic growth rate r for the resource, d1 and d2 for intrinsic mortality rate of sp1 and sp2; a1 and a2 for resource uptake rate of sp1 and sp2
 #' @export
-tilman_comp <- function(time,init,params) {
-  with (as.list(c(time,init,params)), {
-    # description of parameters/state variables:
-    # d1 = intrinsic mortality rate of sp1
-    # a1 = uptake efficiency of sp1
-    # d2 = intrinsic mortality rate of sp2
-    # a2 = uptake efficiency of sp2
-    # r = intrinsic growth rate of resource
+tilman_comp_essential <- function(time,init,params) {
+with (as.list(c(time,init,params)), {
+  # description of parameters/state variables:
+  # d1 = intrinsic mortality rate of sp1
+  # a1 = uptake efficiency of sp1
+  # d2 = intrinsic mortality rate of sp2
+  # a2 = uptake efficiency of sp2
+  # r = intrinsic growth rate of resource
 
-    # Differential equations
-    dN1 <- N1*(a1*R-d1)
-    dN2 <- N2*(a2*R-d2)
-    dR <- r-R*(a1*N1 + a2*N2)
+  # Differential equations
+  dN1 <- min((r1*R1)/(R1+k11)-m1, (r1*R2)/(R2+k12)-m1)
+  dN2 <- min((r2*R1)/(R1+k21)-m2, (r2*R2)/(R2+k22)-m2)
+  dR1 <- a1*(S1-R1) - N1*c11*(dN1/N1 + m1) - N2*c21*(dN2/N2 + m1)
+  dR2 <- a2*(S2-R2) - N1*c12*(dN1/N1 + m2) - N2*c22*(dN2/N2 + m2)
+  # return both dN1 and dN2
+  return(list(c(dN1, dN2, dR1, dR2)))
 
-    # return both dN1 and dN2
-    return(list(c(dN1, dN2, dR)))
+})
+}
 
-  })
+#' Calculate RStar for the 2-consumer, 2-essential resource competition
+#' @param params vector of parameters: k11, k12, k21, k22, r1, r2, m1, m2
+#' @export
+Rstar_essential <- function(params) {
+  R11 = unname(params["k11"] * params["m1"])/(params["r1"] - params["m1"])
+  R12 = unlist(params["k12"] * params["m1"])/(params["r1"] - params["m1"])
+  R21 = unlist(params["k21"] * params["m2"])/(params["r2"] - params["m2"])
+  R22 = unlist(params["k22"] * params["m2"])/(params["r2"] - params["m2"])
+  Rstar_vec <- c(R11 = R11, R12 = R12, R21 = R21, R22 = R22)
+  names(Rstar_vec) <- c("R11", "R12", "R21", "R22")
+  return(Rstar_vec)
 }
--- inst/abiotic_resource_comp/abiotic_rc_script.R
+++ inst/abiotic_resource_comp/abiotic_rc_script.R
@@ -3,51 +3,56 @@ library(tidyr)
 library(ggplot2)
 
 # Write a function that runs the Lotka Volterra competition model
-tilman_comp <- function(time,init,params) {
-  with (as.list(c(time,init,params)), {
-    # description of parameters/state variables:
-    # d1 = intrinsic mortality rate of sp1
-    # a1 = uptake efficiency of sp1
-    # d2 = intrinsic mortality rate of sp2
-    # a2 = uptake efficiency of sp2
-    # r = intrinsic growth rate of resource
 
-    # Differential equations
-    dN1 <- N1*(a1*R-d1)
-    dN2 <- N2*(a2*R-d2)
-    dR <- r-R*(a1*N1 + a2*N2)
-
-    # return both dN1 and dN2
-    return(list(c(dN1, dN2, dR)))
-
-  })
-}
 
 # Set the initial population sizes
-init <- c(N1 = 50, N2 = 25, R = 100)
+init <- c(N1 = 10, N2 = 10, R1 = 20, R2 = 20)
 
 # Set the parameter values
-params <- c(r = .01, d1 = .001, d2 = .001, a1 = .001, a2 = .001)
-
+params <- c(S1 = 12, S2 = 12, r1 = 1.6, r2 = 1, k11 = 18, k12 = 4,
+            k21 = 2, k22 = 14, m1 = .2, m2 = .2, c11 = .25,
+            c12 = .08, c21 = .1, c22 = .2, a1 = .5, a2 = .5)
 # Time over which to simulate model dynamics
-time <- seq(0,750,by = .1)
+time <- seq(0,5000,by = .5)
 
 # Use the tilman_comp function above to run
 # the tilman resource competition model using the
 # parameter estimates defined above
-tilman_out <- data.frame(deSolve::ode(func = ecoevoapps::tilman_comp,
+tilman_out <- data.frame(deSolve::ode(func = ecoevoapps::tilman_comp_essential,
                                       y=init, parms=params, times = time))
+
+tilman_out <- tilman_out %>% dplyr::mutate_if(is.numeric, round, digits = 3)
 
 # Reshape the data so that population sizes of both
 # species are in one column, and an extra column to define
 # species name. This helps with the plotting...
+R11 = (params["k11"] * params["m1"])/(params["r1"] - params["m1"])
+R12 = (params["k12"] * params["m1"])/(params["r1"] - params["m1"])
+R21 = (params["k21"] * params["m2"])/(params["r2"] - params["m2"])
+R22 = (params["k22"] * params["m2"])/(params["r2"] - params["m2"])
 
-tilman_out <- pivot_longer(tilman_out, c(N1,N2,R), "species")
+tilman_out_long <- pivot_longer(tilman_out, c(R1,R2,N1,N2), "species")
 
 # Plot
-ggplot(tilman_out) +
+ggplot(tilman_out_long) +
   geom_line(aes(x = time, y = value, color = species), size = 2) +
   scale_color_brewer(palette = "Set1") +
   ylab("Population size") +
   ecoevoapps::theme_apps()
 
+Rstar_vec <- ecoevoapps::Rstar_essential(params)
+Rstar_df <- data.frame(species = c("N1", "N1", "N2", "N2"),
+                       resource = c("R1", "R2", "R1", "R2"),
+                       Rstar = Rstar_vec)
+Rstar_df2 <- data.frame(species = c("N1", "N2"),
+                        R1star = c(R11, R12),
+                        R2star = c(R21, R22))
+ggplot(tilman_out) +
+  geom_path(aes(x = R1, y = R2)) +
+  geom_segment(data = Rstar_df2, aes(x = R1star, xend = R1star, y = R2star, yend = Inf, color = species)) +
+  geom_segment(data = Rstar_df2, aes(x = R1star, xend = Inf, y = R2star, yend = R2star, color = species)) +
+  scale_color_brewer(name = "Consumer\nspecies", palette = "Set1") +
+  ecoevoapps::theme_apps()
+
+ggplot(tilman_out) +
+  geom_path(aes(x = N1, y = N2))
