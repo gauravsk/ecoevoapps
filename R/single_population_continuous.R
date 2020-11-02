b876c524ae9cad7169bb849eba61b7e054b7297b
Gaurav Kandlikar <gaurav.kandlikar@gmail.com>
2020-11-01 16:02:20 -0800

    define and use run_exponential_model and run_logistic_model

--- R/single_population_continuous.R
+++ R/single_population_continuous.R
@@ -13,6 +13,16 @@ exponential_growth <- function(time,init,params) {
   })
 }
 
+#' Run exponential growth model
+#' @param time vector of time units over which to run model
+#' @param init initial population size of population
+#' @param params intrinsic growth rate r
+#' @export
+run_exponential_model <- function(time,init,params) {
+  deSolve::ode(func = exponential_growth,
+               y = init, times = time, parms = params)
+}
+
 #' Logistic growth model
 #' @param time vector of time units over which to run model
 #' @param init initial population size of population
@@ -41,10 +51,26 @@ lagged_logistic_growth <- function(time, init, params) {
     if (tlag < 0) {
       Nlag <- N1
     } else {
-      Nlag <- lagvalue(tlag)
+      Nlag <- deSolve::lagvalue(tlag)
     }
     dN1 <- r * N1 * (1-(Nlag/K))
     return(list(c(dN1)))
   })
+}
+
+
+#' Run exponential growth model
+#' @param time vector of time units over which to run model
+#' @param init initial population size of population
+#' @param params intrinsic growth rate r
+#' @export
+run_logistic_model <- function(time, init, params) {
+  if("tau" %in% names(params)) {
+    deSolve::dede(func = lagged_logistic_growth,
+                 y = init, times = time, parms = params)
+  } else {
+    deSolve::ode(func = logistic_growth,
+                 y = init, times = time, parms = params)
+  }
 }
 
