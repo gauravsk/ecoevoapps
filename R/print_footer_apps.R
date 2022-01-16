1b2e1ef9ee9a6a1fa769f7d196c6c22be2882870
Gaurav Kandlikar <gaurav.kandlikar@gmail.com>
2022-01-16 12:08:16 -0600

    make a new function for adding descriptive footers in shiny apps

---
+++ R/print_footer_apps.R
@@ -0,0 +1,14 @@
+#' Print footer for Shiny apps
+#'
+#' This function is only intended for use in EcoEvoApps' shiny apps.
+#' @export
+print_app_footer <- function(){
+  cat("#### ***Explore more apps, provide feedback, or contribute!***
+
+*This is an entry in the EcoEvoApps project,
+      a collection of freely available apps to simulate canonical
+      models from theoretical ecology and evolution.
+      Please explore other apps in the collection, provide feedback, or
+      contribute to the project: https://ecoevoapps.gitlab.io,
+      or email us at ecoevoapps@gmail.com.*")
+}
--- inst/app_footer.md
+++
@@ -1,3 +0,0 @@
-#### ***Explore more apps, provide feedback, or contribute!***
-
-*This is an entry in the EcoEvoApps project, a collection of freely available apps to simulate canonical models from theoretical ecology and evolution. Please explore other apps in the collection, provide feedback, or contribute to the project: https://ecoevoapps.gitlab.io, or email us at ecoevoapps@gmail.com.*
