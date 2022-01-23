11f894f56b903eb02648c0dd1138ba61f84af187
Gaurav Kandlikar <gaurav.kandlikar@gmail.com>
2022-01-22 20:11:02 -0600

    add function to run_abiotic_comp_model

--- R/abiotic_resource_competition.R
+++ R/abiotic_resource_competition.R
@@ -2,31 +2,36 @@
 #' @param time vector of time units over which to run model
 #' @param init initial population size of population
 #' @param params vector of parameters: intrinsic growth rate r for the resource, d1 and d2 for intrinsic mortality rate of sp1 and sp2; a1 and a2 for resource uptake rate of sp1 and sp2
-#' @export
 tilman_comp_essential <- function(time,init,params) {
-with (as.list(c(time,init,params)), {
-  # description of parameters/state variables:
-  # d1 = intrinsic mortality rate of sp1
-  # a1 = uptake efficiency of sp1
-  # d2 = intrinsic mortality rate of sp2
-  # a2 = uptake efficiency of sp2
-  # r = intrinsic growth rate of resource
+  with (as.list(c(time,init,params)), {
+    # description of parameters/state variables:
+    # d1 = intrinsic mortality rate of sp1
+    # a1 = uptake efficiency of sp1
+    # d2 = intrinsic mortality rate of sp2
+    # a2 = uptake efficiency of sp2
+    # r = intrinsic growth rate of resource
 
-  # Differential equations
-  dN1 <- N1*(min((r1*R1)/(R1+k11)-m1, (r1*R2)/(R2+k12)-m1))
-  dN2 <- N2*(min((r2*R1)/(R1+k21)-m2, (r2*R2)/(R2+k22)-m2))
-  dR1 <- a1*(S1-R1) - N1*c11*(dN1/N1 + m1) - N2*c21*(dN2/N2 + m2)
-  dR2 <- a2*(S2-R2) - N1*c12*(dN1/N1 + m1) - N2*c22*(dN2/N2 + m2)
-  # return both dN1 and dN2
-  return(list(c(dN1, dN2, dR1, dR2)))
-
-})
+    # Differential equations
+    dN1 <- N1*(min((r1*R1)/(R1+k11)-m1, (r1*R2)/(R2+k12)-m1))
+    dN2 <- N2*(min((r2*R1)/(R1+k21)-m2, (r2*R2)/(R2+k22)-m2))
+    dR1 <- a1*(S1-R1) - N1*c11*(dN1/N1 + m1) - N2*c21*(dN2/N2 + m2)
+    dR2 <- a2*(S2-R2) - N1*c12*(dN1/N1 + m1) - N2*c22*(dN2/N2 + m2)
+    # return both dN1 and dN2
+    return(list(c(dN1, dN2, dR1, dR2)))
+  })
 }
 
 #' Calculate RStar for the 2-consumer, 2-essential resource competition
 #' @param params vector of parameters: k11, k12, k21, k22, r1, r2, m1, m2
 #' @export
-Rstar_essential <- function(params) {
+run_abiotic_comp_rstar <- function(params) {
+
+  if(!(all(names(init) %in% c("k11", "k12", "k21", "k22",
+                              "r1", "r2", "m1", "m2")))) {
+    stop("init should be a numeric vector of length 4, e.g. c(N1 = 10, N2 = 20, R1 = 20, R2 = 20)")
+  }
+
+
   R11 = unname(params["k11"] * params["m1"])/(params["r1"] - params["m1"])
   R12 = unlist(params["k12"] * params["m1"])/(params["r1"] - params["m1"])
   R21 = unlist(params["k21"] * params["m2"])/(params["r2"] - params["m2"])
@@ -35,3 +40,60 @@ Rstar_essential <- function(params) {
   names(Rstar_vec) <- c("R11", "R12", "R21", "R22")
   return(Rstar_vec)
 }
+
+
+#' Run the Tilman abiotic resource competition model
+#' @param time vector of time units over which to run model, starting from 0.
+#' `time` can also be supplied as just the total length of the simulation (i.e. tmax)
+#' @param init vector of initial population sizes for both species, with names N1 and N2
+#' @param params vector of model parameters (`S1`, `S2`, `r1`, `r2`, `k11`, `k12`,
+#' `k21`, `k22`, `m1`, `m2`, `c11`, `c12`,
+#' `c21`, `c22`, `a1`, `a1`)
+#' @examples
+#' # Define full time series, and run model in terms of carrying capacities
+#' # and relative competitive effects
+#' run_abiotic_comp_model(time = seq(0,100,0.1),
+#'  init = c(R1 = 20, R2 = 20,N1 = 10, N2 = 10),
+#'  params = c(S1 = 12, S2 = 12, r1 = 1.6, r2 = 1,
+#' k11 = 18, k12 = 4, k21 = 2, k22 = 14,
+#' m1 = .2, m2 = .2,c11 = .25, c12 = .08,
+#' c21 = .1, c22 = .2, a1 = .5, a2 = .5))
+#' @export
+run_abiotic_comp_model <- function(time = seq(0,100,0.1),
+                                   init = c(R1 = 20, R2 = 20,
+                                            N1 = 10, N2 = 10),
+                                   params = c(S1 = 12, S2 = 12,
+                                              r1 = 1.6, r2 = 1,
+                                              k11 = 18, k12 = 4,
+                                              k21 = 2, k22 = 14,
+                                              m1 = .2, m2 = .2,
+                                              c11 = .25, c12 = .08,
+                                              c21 = .1, c22 = .2,
+                                              a1 = .5, a2 = .5)) {
+  # Check how time has been defined (if just Tmax, then make vector)
+  # and if vector was supplied, check that it starts at t = 0
+  if(length(time) == 1) {
+    tmax <- time
+    time <- seq(0, tmax, 0.1)
+  } else if(time[1] != 0) {
+    stop("The time vector should start at 0.")
+  }
+
+  # Check that init has been defined properly
+  if(!(is.numeric(init))) stop("init should be a numeric vector of length 4, e.g. c(N1 = 10, N2 = 20, R1 = 20, R2 = 20)")
+  if(length(init) != 4) stop("init should be a numeric vector of length 4, e.g. c(N1 = 10, N2 = 20, R1 = 20, R2 = 20)")
+  if(!(all(names(init) %in% c("N1","N2", "R1","R2")))) stop("init should be a numeric vector of length 4, e.g. c(N1 = 10, N2 = 20, R1 = 20, R2 = 20)")
+
+  # Check that params is correctly defined (just r)
+  if(!(is.numeric(params))) stop("params should be a numeric vector")
+  if(!(all(c("S1", "S2", "r1", "r2", "k11", "k12",
+             "k21", "k22", "m1", "m2", "c11", "c12",
+             "c21", "c22", "a1", "a1") %in%
+           names(params)))) {
+    stop("Please provide a complete parameter vector (see ?run_abiotic_comp_model for details)")
+  }
+
+  deSolve::ode(func = tilman_comp_essential,
+               y = init, parms=params, times = time)
+}
+
